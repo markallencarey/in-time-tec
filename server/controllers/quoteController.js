b71e7ba9cd1c399d5606dadecc320970b300f29d
Mark Carey <markallencarey@gmail.com>
2021-03-30 23:44:52 -0600

    add new api

--- server/controllers/quoteController.js
+++ server/controllers/quoteController.js
@@ -2,10 +2,10 @@ const axios = require('axios')
 
 module.exports = {
   getQuote: async (req, res) => {
-    await axios.get('http://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand').then(quote => {
+    await axios.get('https://api.quotable.io/random').then(quote => {
       res.status(200).send({
-        author: quote.data[0].title.rendered,
-        text: quote.data[0].content.rendered
+        author: quote.data.author,
+        text: quote.data.content
       })
     }).catch(err => {
       console.log(err)
